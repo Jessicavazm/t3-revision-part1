c24f701a26c601755e7f7f1df7e87505e1edf940
Jessicavazm <jjessicavaz@gmail.com>
2024-12-28 10:03:14 +1100

    feat(dice): Encapsulated try-catch in a function block

--- src/diceRoller_week2.js
+++ src/diceRoller_week2.js
@@ -1,28 +1,27 @@
 // import prompt packages
 // const inquirer = require('inquirer').default;]
-const promptSync = require('prompt-sync')();
+const promptSync = require("prompt-sync")();
 
 // ensure file has been linked or started correctly
-console.log('Dice Roller Application starting...')
-
+console.log("Dice Roller Application starting...");
 
 /**
  * Roll a random dice side from a variable-sided dice
  * @param {number} [diceSide=6] The total number of sides in the dice
  * @returns {number} The side of the dice rolled
  */
-function rollDice(diceSide=6) {
-    if (diceSide <=0) {
-        throw new Error ('Dice must be greater than 0.')
-    }
-    let diceResult = 0;
-    diceResult = Math.ceil(Math.random() * diceSide);
-    return diceResult;
+function rollDice(diceSide = 6) {
+  if (diceSide <= 0) {
+    throw new Error("Dice side must be greater than 0.");
+  }
+  let diceResult = 0;
+  diceResult = Math.ceil(Math.random() * diceSide);
+  return diceResult;
 }
 
 // console.log("The side you rolled is: " + rollDice());
 
-// Inquirer
+// Using Inquirer package
 // const prompt = inquirer.createPromptModule();
 
 // prompt([
@@ -36,18 +35,22 @@ function rollDice(diceSide=6) {
 // });
 
 
-// PromptSync
-const diceSide = promptSync("How many sided dice do you want to roll? ")
+// Using PromptSync package
+function getDiceSide() {
+  const diceSide = promptSync("How many sided dice do you want to roll? ");
 
-try {
+  try {
     //
-    const parsedDiceSide = parseInt(diceSide); 
+    const parsedDiceSide = parseInt(diceSide);
     if (isNaN(parsedDiceSide)) {
-        throw new Error ('Please enter a valid number.');
+      throw new Error("Please enter a valid number.");
     }
     const diceResult = rollDice(diceSide);
     console.log(`You rolled a ${diceResult}-sided dice.`);
-} catch (error){
-    console.error('You got an error!');
+  } catch (error) {
+    console.error("You got an error!");
     console.log(error); // this displays the error stack trace
+  }
 }
+
+getDiceSide();
