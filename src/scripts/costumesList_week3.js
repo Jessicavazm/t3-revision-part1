590046a21811b271a8a8842c2fd022641511607d
Jessicavazm <jjessicavaz@gmail.com>
2024-12-30 22:12:12 +1100

    (costumeApp)feat: Created an unordered list for Halloween Costumes

--- src/scripts/costumesList_week3.js
+++ src/scripts/costumesList_week3.js
@@ -12,14 +12,32 @@ let dataArray = [
     "Clowns"
 ];
 
-
-dataArray.forEach((costume) => {
-    console.log(costume);
-});
-
-
 // Fetch the element and stores it in a variable
 let costumesContainer = document.getElementById("costumeContainer");
 
+// Create an unordered list tag, ensure you store element in variable
+let costumeContainerList = document.createElement("ul");
+
 // Add text to the container
-costumesContainer.innerText = "Hello World!";
+// costumesContainer.innerText = "Hello World!";
+
+// Loop to print each costume
+dataArray.forEach((costume) => {
+    // Verify what we are working with
+    console.log(costume);
+
+    // Create the list element
+    let newCostumeEntry = document.createElement("li");
+
+    // Add the value to the HTML element
+    newCostumeEntry.innerText = costume;
+
+    // Append the element to the container
+    // Here you are appending the newCostumeEntry to the costumeContainerList element
+    costumeContainerList.appendChild(newCostumeEntry);
+});
+
+// Add the container to the HTML Page
+// Here you are appending the costumeContainerList to the costumeContainer element
+costumeContainer.appendChild(costumeContainerList);
+
