8d4a541163f6c8f0c2b8297268da1cee670027a7
Jessicavazm <jjessicavaz@gmail.com>
2025-01-05 00:24:57 +1100

    (PokemonEncounter)feat: Created a li for Pokemon types

--- src/scripts/pokemonEncounter_wk4.js
+++ src/scripts/pokemonEncounter_wk4.js
@@ -3,15 +3,56 @@ console.log('Pokemon journey begins...')
 // Fetch elements
 const encounterButton = document.getElementById('pokemonEncounterButton');
 const PokemonRenderArea = document.getElementById('encounteredPokemonArea');
+const pokemonContainerDiv = document.getElementById('pokemonContainer');
 
+// Function to fetch Pokemon data and display in Front-end 
 function renderPokemonData(pokemonData){
-    PokemonRenderArea.innerText = pokemonData.name;
+    if (!pokemonData.name) {
+        return;
+    }
+    // add a class to pokemonContainerDiv to style it
+    pokemonContainerDiv.classList += 'pokemonCardEntry';
+
+    // create HTML elements and append it
+    let pokemonImage = document.createElement('img');
+    // .src is the path to the path to pokemon img
+    pokemonImage.src = pokemonData.image;
+    pokemonContainerDiv.appendChild(pokemonImage);
+
+    let pokemonHeading = document.createElement('h1');
+    pokemonHeading.innerText = pokemonData.name;
+    pokemonContainerDiv.appendChild(pokemonHeading);
+
+    let pokemonTypesHeading = document.createElement('h3');
+    pokemonTypesHeading.innerText = 'Types: ';
+    pokemonContainerDiv.appendChild(pokemonTypesHeading);
+
+    let pokemonTypeList = document.createElement('ul');
+    // Loop through the array of pokemonData
+    pokemonData.types.forEach((typeObject) => {
+        // Create an element for each type
+        let pokemonTypeListItem = document.createElement('li');
+        // Add name to li
+        pokemonTypeListItem.innerText = typeObject.type.name;
+        // Append it to the ul
+        pokemonTypeList.appendChild(pokemonTypeListItem);
+    })
+
+    // Append the element to the div
+    pokemonContainerDiv.appendChild(pokemonTypeList)
 }
 
+// Function to get a random pokemon
+function getRandomPokemonId() {
+    // Random number between 1 and 1025 (max number of Pokemon)
+    return Math.ceil(Math.random() * 1025);
+}
+
+// Function to fetch the pokemon from API
 async function getPokemon() {
     console.log('Looking for a wild Pokemon');
 
-    let apiResponse = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
+    let apiResponse = await fetch('https://pokeapi.co/api/v2/pokemon/' + getRandomPokemonId());
     let apiData =  await apiResponse.json();
 
     // Fetch name, type, image, cry
@@ -24,6 +65,7 @@ async function getPokemon() {
     };
 }
 
+// Event listener for Pokemon Button, third option used for more than one stmt
 // encounterButton.addEventListener('click', (event) => getPokemon(event));
 
 // encounterButton.addEventListener('click', getPokemon);
